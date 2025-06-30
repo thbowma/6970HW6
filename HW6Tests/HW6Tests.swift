68df896d535d4f76a9f24db31b0d4af28caa8bbf
Thomas Bowman <thomas@Thomass-MacBook-Pro-2.local>
2025-06-29 23:44:39 -0500

    Inital commit

---
+++ HW6Tests/HW6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HW6Tests.swift
+//  HW6Tests
+//
+//  Created by Thomas Bowman on 6/29/25.
+//
+
+import Testing
+@testable import HW6
+
+struct HW6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
